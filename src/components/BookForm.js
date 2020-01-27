57b9f0e10f8c2de6de9c934578ded4453766a340
Mohammed Basha <mohammedbasha9191@gmail.com>
2020-01-28 00:40:21 +0200

    Adding the BookForm component to add new books

---
+++ src/components/BookForm.js
@@ -0,0 +1,25 @@
+import React, {useContext, useState} from 'react'
+import {BookContext} from '../contexts/BookContext'
+
+const NewBookForm = _ => {
+    const {addBook} = useContext(BookContext),
+          [title, setTitle] = useState(''),
+          [author, setAuthor] = useState(''),
+          handleSubmit = e => {
+              e.preventDefault()
+              addBook(title, author)
+              setTitle('')
+              setAuthor('')
+          }
+    return (
+        <form onSubmit={handleSubmit}>
+            <input type="text" placeholder="Type a title" value={title}
+                onChange={e => setTitle(e.target.value)} required />
+            <input type="text" placeholder="The author" value={author}
+                onChange={e => setAuthor(e.target.value)} required />
+            <input type="submit" value="Add a book" />
+        </form>
+    )
+}
+
+export default NewBookForm
